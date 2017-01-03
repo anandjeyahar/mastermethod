f8bf40f159c73a45d725d6989f9940760bef104f
anand <anand.jeyahar@gmail.com>
2017-01-03 20:45:54 +0530

    minor additions to readme

--- version.py
+++ version.py
@@ -3,7 +3,7 @@
 short_version = '1.0.0'
 version = '1.0.0'
 full_version = '1.0.0'
-git_revision = 'ff8c4bdcc1fe2152931ffea33c66d4ce4cacd9e7'
+git_revision = '4032a1084590a81be7c15ec08d61b046e982661c'
 release = True
 
 if not release:
