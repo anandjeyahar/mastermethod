47ac2d39ba3132db219c5617e61a0bb8f092aef3
anand <anand.jeyahar@gmail.com>
2017-01-06 14:52:01 +0530

    minor fixes

--- mastermethod.py
+++ mastermethod.py
@@ -1,17 +1,6 @@
 import operator
 from inspect import signature
 
-def masterMethod(lst, comp=operator.gt, mergeFunc=None):
-    #todo: add assert that cmp is a binary operator
-    #todo: add assert for lst contains orderable types
-    length = int(len(lst))
-    if length > 1:
-        lst1, lst2 = lst[:int(length/2)], lst[int(length/2):]
-        masterMethod(lst1, comp=comp)
-        masterMethod(lst2, comp=comp)
-
-        return merge(lst, lst1, lst2,comp=comp)
-    return False
 
 def merge(lst, lst1, lst2, comp):
     i=0
@@ -38,12 +27,24 @@ def merge(lst, lst1, lst2, comp):
         k=k+1
     return lst
 
+def masterMethod(lst, comp=operator.gt, mergeFunc=merge):
+    #todo: add assert that cmp is a binary operator
+    #todo: add assert for lst contains orderable types
+    length = int(len(lst))
+    if length > 1:
+        lst1, lst2 = lst[:int(length/2)], lst[int(length/2):]
+        masterMethod(lst1, comp=comp)
+        masterMethod(lst2, comp=comp)
+
+        return mergeFunc(lst, lst1, lst2,comp=comp)
+    return False
+
 if __name__ == '__main__':
     alist = [54,26,93,17,77,31,44,55,20]
     print("Original inptu")
     print(alist)
-    print("Sorted ascending")
-    print(masterMethod(alist))
     print("Sorted descending")
+    print(masterMethod(alist))
+    print("Sorted ascending")
     print(masterMethod(alist,comp=operator.lt))
 
